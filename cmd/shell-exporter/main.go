fb5ed2f552ace8085dd35b0a011b6a5f32f5ba8f
Vitaly Uvarov <vitalyu@gmail.com>
2021-04-07 14:31:39 +0000

    appVersion

--- cmd/shell-exporter/main.go
+++ cmd/shell-exporter/main.go
@@ -11,16 +11,26 @@ import (
 	"net/http"
 )
 
+var appVersion = "0.0.000" // go build -ldflags "-X main.appConfigVersion=1.2.345"
+
 func processAppArguments() (scriptsDir string, port int) {
-	var needHelp bool
-	flag.StringVar(&scriptsDir, "f", "../metrics", "scripts dir")
+	var showHelpInfo bool
+	var showAppVersion bool
+
+	flag.StringVar(&scriptsDir, "f", "metrics", "scripts dir")
 	flag.IntVar(&port, "port", 9360, "exporter port")
-	flag.BoolVar(&needHelp, "help", false, "help info")
+	flag.BoolVar(&showHelpInfo, "help", false, "help info")
+	flag.BoolVar(&showAppVersion, "version", false, "app version info")
 	flag.Parse()
 
-	if needHelp {
+	if showHelpInfo {
 		fmt.Printf("Usage of %s:\n", path.Base(os.Args[0]))
 		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	if showAppVersion {
+		fmt.Println(appVersion)
 		os.Exit(0)
 	}
 
