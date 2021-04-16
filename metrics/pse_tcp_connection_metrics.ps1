fbed1640eb5290cdf039aebf209190bf48c3e4a8
Renat Shajmardanov <r.shajmardanov@dodopizza.com>
2021-04-16 14:29:17 +0300

    #3 improve script speed

--- metrics/pse_tcp_connection_metrics.ps1
+++ metrics/pse_tcp_connection_metrics.ps1
@@ -1,22 +1,30 @@
-$services = Get-Process -IncludeUserName | Where-Object {$_.ProcessName -eq "w3wp"} 
-$connections = Get-NetTCPConnection
-@(
-	ForEach ($service in $services) 
-	{
-		$connections `
-			| Where-Object {$service.Id -eq $_.OwningProcess} `
-			| Group-Object -Property OwningProcess, State `
-			| Select -Property Count, Name, @{Name="Service";Expression={ $service.UserName }} `
-			| ForEach-Object {
-				@{
-					# metric value
-					'value' = $_.Count
-					# metric labels
-					'labels' = @{
-						'state' = ($_.Name -split ', ')[1]
-						'service' = ($_.Service -split '\\')[1]
-					}
-				}
-			}
+function Get-PidToNameMap($processes) {
+	$hash = @{}
+	$processes | ForEach-Object {
+		$hash[$_.Id.ToString()] = ($_.UserName -split '\\')[1]
 	}
-) | ConvertTo-Json -Compress
+	return $hash
+}
+$pidToName = Get-PidToNameMap(Get-Process -IncludeUserName `
+								| Where-Object { $_.ProcessName -eq "w3wp" } `
+								| Select-Object -Property Id, UserName)
+
+Get-NetTCPConnection `
+| Group-Object -Property OwningProcess, State `
+| Select-Object -Property Count, `
+							@{Name = "OwningProcess"; Expression = { $_.Group[0].OwningProcess.ToString() } }, `
+							@{Name = "State"; Expression = { $_.Group[0].State.ToString() } } `
+| Sort-Object -Property OwningProcess, State `
+| ForEach-Object {
+	if ($pidToName.ContainsKey($_.OwningProcess)) {
+		@{
+			# metric value
+			'value'  = $_.Count
+			# metric labels
+			'labels' = @{
+				'state'   = $_.State
+				'service' = $pidToName[$_.OwningProcess]
+			}
+		}
+	}
+} | ConvertTo-Json -Compress
