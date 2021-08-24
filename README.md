f0dd501edf2536c0243c42bbf9ed38d461f6710e
Roman Martsev <r.martsev@dodopizza.com>
2021-08-24 12:40:49 +0000

    Changes in bash script

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Shell Exporter can execute `Powershell` or `Bash` scripts and transform its outp
 Shell Exporter exposes metrics based on your `Powershell` or `Bash` scripts names. Examples of scripts can be finded at `metrics` folder
 
 | Script name                                      | Metric name                                  |
-| -------------------------------------------------|----------------------------------------------|
+| ------------------------------------------------ | -------------------------------------------- |
 | `bash_gauge.sh`                                  | `bash_gauge`                                 |
 | `pse_tcp_connection_metrics.ps1`                 | `pse_tcp_connection_metrics`                 |
 | `pse_tcp_dynamic_port_range_number_of_ports.ps1` | `pse_tcp_dynamic_port_range_number_of_ports` |
@@ -15,22 +15,21 @@ Shell Exporter exposes metrics based on your `Powershell` or `Bash` scripts name
 ## Startup options
 
 | Option    | Default value | Description           |
-| ----------|---------------|-----------------------|
+| --------- | ------------- | --------------------- |
 | --f       | `./metrics`   | scripts directory     |
 | --port    | 9360          | exporter port         |
 | --help    | -             | show help             |
 | --version | -             | show exporter version |
-
 
 ## Installing as Windows Service
 
 1. Download binary
 2. Install [nssm](https://nssm.cc/) via [Chocolatey](https://community.chocolatey.org/packages/NSSM)
 
-| Action    | Command                                                          |
-| ----------|------------------------------------------------------------------|
-| install   | nssm install shell_exporter C:\shell-exporter.exe -f C:\scripts  |
-| remove    | nssm remove shell_exporter confirm                               |
+| Action  | Command                                                         |
+| ------- | --------------------------------------------------------------- |
+| install | nssm install shell_exporter C:\shell-exporter.exe -f C:\scripts |
+| remove  | nssm remove shell_exporter confirm                              |
 
 ## Development
 
@@ -41,10 +40,10 @@ Shell Exporter exposes metrics based on your `Powershell` or `Bash` scripts name
 
 ### Makefile targets
 
-| Target    | Action                                                                                  |
-| ----------|-----------------------------------------------------------------------------------------|
-| build     | build binary for `linux` and `windows` platforms                                        |
-| tidy      | remove all dependencies from the go.mod file which are not required in the source files |
-| test      | run unit tests                                                                          |
-| lint      | run linting                                                                             |
-| run       | run exporter locally                                                                    |
+| Target | Action                                                                                  |
+| ------ | --------------------------------------------------------------------------------------- |
+| build  | build binary for `linux` and `windows` platforms                                        |
+| tidy   | remove all dependencies from the go.mod file which are not required in the source files |
+| test   | run unit tests                                                                          |
+| lint   | run linting                                                                             |
+| run    | run exporter locally                                                                    |
--- metrics/gogetssl_domains_expired_days.sh
+++ metrics/gogetssl_domains_expired_days.sh
@@ -1,29 +1,107 @@
 #!/bin/bash
 V1_URL_PREFIX=https://my.gogetssl.com/api/
-V1_API_USER=***
-V1_API_PASS=***
+V1_API_USER=
+V1_API_PASS=
+V1_API_TOKEN=
 
-V1_API_TOKEN=$(
-    curl --silent --location -g --request POST "${V1_URL_PREFIX}/auth" \
-        --header 'Content-Type: application/x-www-form-urlencoded' \
-        --data-urlencode "user=${V1_API_USER}" \
-        --data-urlencode "pass=${V1_API_PASS}" |
-        jq -r '.key'
-)
+# Input parameters:
+# $1 - URL prefix
+# $2 - API user
+# $3 - API password
+# Output:
+# 0 - Everything is correct
+# 1 - invalid arguments number
+# 2 - empty answer from server
+function getApiToken {
+    if [ $# -ne 3 ]; then
+        return 1
+    else
+        result=$(
+            curl --silent --location -g --request POST "${1}/auth" \
+                --header 'Content-Type: application/x-www-form-urlencoded' \
+                --data-urlencode "user=${2}" \
+                --data-urlencode "pass=${3}" |
+                jq -r '.key' 2> /dev/null
+        )
+        if [[ ${result} == "" ]]; then
+            return 2
+        else
+            echo "${result}"
+            return 0
+        fi
+    fi
+}
 
-active_orders=$(
-    curl --silent --location -g --request GET "${V1_URL_PREFIX}/orders/ssl/all?auth_key=${V1_API_TOKEN}&limit=999" |
-        jq -r '.orders[] | select(.status == "active").order_id'
-)
+# Input parameters:
+# $1 - URL prefix
+# $2 - API token
+# Output:
+# 0 - Everything is correct
+# 1 - invalid arguments number
+# 2 - empty answer from server
+function getActiveOrders {
+    if [ $# -ne 2 ]; then
+        return 1
+    else
+        result=$(
+            curl --silent --location -g --request GET "${1}/orders/ssl/all?auth_key=${2}&limit=999" |
+                jq -r '.orders[] | select(.status == "active").order_id' 2> /dev/null
+            )
+        if [[ ${result} == "" ]]; then
+            return 2
+        else
+            echo "${result}"
+            return 0
+        fi
+    fi
+}
 
-(
-    for order_id in ${active_orders}; do
-        curl --silent --location -g --request GET "${V1_URL_PREFIX}/orders/status/${order_id}?auth_key=${V1_API_TOKEN}" | 
-        jq '{ 
-            value: (((((.valid_till + "T00:00:00Z")|fromdate) - now) / 60 / 60 / 24)|round), 
-            labels: { 
-                domain: (.domain, .san[].san_name) 
-            } 
-        }'
-    done 
-) | jq -s
+# Input parameters:
+# $1 - active orders
+# $2 - URL prefix
+# $3 - API token
+# Output:
+# 0 - Everything is correct
+# 1 - invalid arguments number
+# 2 - empty answer from server
+function ordersToJson {
+    if [ $# -ne 3 ]; then
+        return 1
+    else
+        for order_id in $1; do
+            result+=$(curl --silent --location -g --request GET "${2}/orders/status/${order_id}?auth_key=${3}" | 
+                jq '{ 
+                    value: (((((.valid_till + "T00:00:00Z")|fromdate) - now) / 60 / 60 / 24)|round), 
+                    labels: { 
+                        domain: (.domain, .san[].san_name) 
+                    } 
+                }')
+        done
+        if [[ ${result} == "" ]]; then
+            return 2
+        else
+            echo ${result}
+            return 0
+        fi
+    fi
+}
+
+if [[ $1 == "token" ]]; then
+    echo $(getApiToken ${V1_URL_PREFIX} ${V1_API_USER} ${V1_API_PASS})
+else
+    if [[ ${V1_API_TOKEN} != "" ]]; then
+        activeOrders=$(getActiveOrders ${V1_URL_PREFIX} ${V1_API_TOKEN})
+        activeOrdersCode=$?
+    fi
+
+    if [[ ${activeOrdersCode} == 2 || ${V1_API_TOKEN} == "" ]]; then
+        token=$(getApiToken ${V1_URL_PREFIX} ${V1_API_USER} ${V1_API_PASS})
+        tokenCode=$?
+        if [[ ${tokenCode} == 0 ]]; then
+            V1_API_TOKEN=${token}
+            activeOrders=$(getActiveOrders ${V1_URL_PREFIX} ${V1_API_TOKEN})
+            activeOrdersCode=$?
+        fi
+    fi
+    echo $(ordersToJson "${activeOrders[*]}" "${V1_URL_PREFIX}" "${V1_API_TOKEN}")
+fi
