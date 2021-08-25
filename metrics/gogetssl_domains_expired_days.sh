728f288ca976644c8b50625fe8395269c2c2d6b1
Roman Martsev <r.martsev@dodopizza.com>
2021-08-25 14:33:37 +0300

    Changes in gogetssl exporter script

--- metrics/gogetssl_domains_expired_days.sh
+++ metrics/gogetssl_domains_expired_days.sh
@@ -1,107 +1,130 @@
 #!/bin/bash
+set -uo pipefail
+
 V1_URL_PREFIX=https://my.gogetssl.com/api/
 V1_API_USER=
 V1_API_PASS=
-V1_API_TOKEN=
 
-# Input parameters:
-# $1 - URL prefix
-# $2 - API user
-# $3 - API password
-# Output:
-# 0 - Everything is correct
-# 1 - invalid arguments number
-# 2 - empty answer from server
+# Usage: serverErrorHandler <Server responce>
+function serverErrorHandler {
+    [ $# -ne 1 ] && return 99
+    local result=${1}
+    local error=$(echo "${result}" | jq -r '.error')
+    local errorMessage=$(echo "${result}" | jq -r '.message')
+    local errorDescription=$(echo "${result}" | jq -r '.description')
+    local resultCode=0
+    if [[ ${error} == true ]]; then
+        case "${message}" in
+        "Wrong username/password" ) resultCode=101
+        ;;
+        "Auth key required for this method" ) resultCode=102
+        ;;
+        * ) resultCode=100
+        ;;
+        esac
+    fi
+    [[ ${error} == null && ${result} != null && ${result} != "" ]] && echo "${result}"
+    return ${resultCode}
+}
+
+# Usage: getApiToken <URL prefix> <API user> <API password>
 function getApiToken {
-    if [ $# -ne 3 ]; then
-        return 1
-    else
-        result=$(
-            curl --silent --location -g --request POST "${1}/auth" \
-                --header 'Content-Type: application/x-www-form-urlencoded' \
-                --data-urlencode "user=${2}" \
-                --data-urlencode "pass=${3}" |
-                jq -r '.key' 2> /dev/null
+    [ $# -ne 3 ] && return 99
+    local urlPrefix=${1}
+    local apiUser=${2}
+    local apiPassword=${3}
+    local resultCode=0
+    local result=$(serverErrorHandler $(
+        curl --silent --location -g --request POST "${urlPrefix}/auth" \
+            --header 'Content-Type: application/x-www-form-urlencoded' \
+            --data-urlencode "user=${apiUser}" \
+            --data-urlencode "pass=${apiPassword}"
         )
-        if [[ ${result} == "" ]]; then
-            return 2
-        else
-            echo "${result}"
-            return 0
-        fi
-    fi
+    )
+    resultCode=$?
+    [ ${resultCode} -eq 0 ] && (echo "${result}" | jq -r '.key')
+    return ${resultCode}
 }
 
-# Input parameters:
-# $1 - URL prefix
-# $2 - API token
-# Output:
-# 0 - Everything is correct
-# 1 - invalid arguments number
-# 2 - empty answer from server
+# Usage: getActiveOrders <URL prefix> <API token>
 function getActiveOrders {
-    if [ $# -ne 2 ]; then
-        return 1
-    else
-        result=$(
-            curl --silent --location -g --request GET "${1}/orders/ssl/all?auth_key=${2}&limit=999" |
-                jq -r '.orders[] | select(.status == "active").order_id' 2> /dev/null
-            )
-        if [[ ${result} == "" ]]; then
-            return 2
-        else
-            echo "${result}"
-            return 0
-        fi
-    fi
+    [ $# -ne 2 ] && return 99
+    local urlPrefix=${1}
+    local apiToken=${2}
+
+    local result=$(serverErrorHandler $(curl --silent --location -g --request GET "${urlPrefix}/orders/ssl/all?auth_key=${apiToken}&limit=999"))
+    resultCode=$?
+    [ ${resultCode} -eq 0 ] && (echo "${result}" | jq -r '.orders[] | select(.status == "active").order_id')
+    return ${resultCode}
 }
 
-# Input parameters:
-# $1 - active orders
-# $2 - URL prefix
-# $3 - API token
-# Output:
-# 0 - Everything is correct
-# 1 - invalid arguments number
-# 2 - empty answer from server
+# Usage: ordersToJson <active orders> <URL prefix> <API token>
 function ordersToJson {
-    if [ $# -ne 3 ]; then
-        return 1
-    else
-        for order_id in $1; do
-            result+=$(curl --silent --location -g --request GET "${2}/orders/status/${order_id}?auth_key=${3}" | 
+    [ $# -ne 3 ] && return 99
+    local activeOrders=${1}
+    local urlPrefix=${2}
+    local apiToken=${3}
+    (
+        for orderId in ${activeOrders}; do
+            local result=$(curl --silent --location -g --request GET "${urlPrefix}/orders/status/${orderId}?auth_key=${apiToken}")
+            local error=$(echo "${result}" | jq -r '.error')
+            [[ ${error} != true ]] && (echo -n "${result}" |
                 jq '{ 
                     value: (((((.valid_till + "T00:00:00Z")|fromdate) - now) / 60 / 60 / 24)|round), 
                     labels: { 
                         domain: (.domain, .san[].san_name) 
                     } 
-                }')
+                }'
+            )
         done
-        if [[ ${result} == "" ]]; then
-            return 2
-        else
-            echo ${result}
-            return 0
-        fi
-    fi
+    ) | jq -s
+    return 0
 }
 
-if [[ $1 == "token" ]]; then
-    echo $(getApiToken ${V1_URL_PREFIX} ${V1_API_USER} ${V1_API_PASS})
-else
-    if [[ ${V1_API_TOKEN} != "" ]]; then
-        activeOrders=$(getActiveOrders ${V1_URL_PREFIX} ${V1_API_TOKEN})
-        activeOrdersCode=$?
-    fi
-
-    if [[ ${activeOrdersCode} == 2 || ${V1_API_TOKEN} == "" ]]; then
-        token=$(getApiToken ${V1_URL_PREFIX} ${V1_API_USER} ${V1_API_PASS})
-        tokenCode=$?
-        if [[ ${tokenCode} == 0 ]]; then
-            V1_API_TOKEN=${token}
-            activeOrders=$(getActiveOrders ${V1_URL_PREFIX} ${V1_API_TOKEN})
-            activeOrdersCode=$?
+function main {
+    local result=""
+    local resultCode=0
+    case "${1:-}" in
+    "token" )
+        result=$(getApiToken ${V1_URL_PREFIX} ${V1_API_USER} ${V1_API_PASS})
+        resultCode=$?
+    ;;
+    * )
+        # Get token if not set
+        if [[ ${V1_API_TOKEN:-} == "" ]]; then 
+            echo "--1--"
+            export V1_API_TOKEN=$(main "token")
+            resultCode=$?
         fi
-    fi
-    echo $(ordersToJson "${activeOrders[*]}" "${V1_URL_PREFIX}" "${V1_API_TOKEN}")
-fi
+        # Get list of orders
+        if [ ${resultCode} -eq 0 ]; then
+            echo "--2--"
+            activeOrders=$(getActiveOrders ${V1_URL_PREFIX} ${V1_API_TOKEN}) 
+            resultCode=$?
+            # If get server error, trying to get token
+            if [ ${resultCode} -ne 0 ]; then
+                echo "--3--"
+                export V1_API_TOKEN=$(main "token")
+                resultCode=$?
+                # If success token query
+                if [ ${resultCode} -eq 0 ]; then
+                    echo "--4--"
+                    activeOrders=$(getActiveOrders ${V1_URL_PREFIX} ${V1_API_TOKEN}) 
+                    resultCode=$?
+                fi
+            fi
+        fi
+        # If order list is present
+        if [ ${resultCode} -eq 0 ]; then
+            echo "--5--"
+            result=$(ordersToJson "${activeOrders[*]}" "${V1_URL_PREFIX}" "${V1_API_TOKEN}")
+            resultCode=$?
+        fi
+    ;;
+    esac
+
+    [ ${resultCode} -eq 0 ] && echo ${result} && return 0
+    [ ${resultCode} -ne 0 ] && return ${resultCode}
+}
+
+main $@
